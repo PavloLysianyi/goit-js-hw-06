3af437ee1c5b351ce6690ea46dd1bacb283b07fe
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-12-01 15:19:36 +0200

    corrected

--- js/task-02.js
+++ js/task-02.js
@@ -8,9 +8,12 @@ const ingredients = [
 ];
 
 const ingredientsList = document.getElementById("ingredients");
-ingredients.forEach((ingredient) => {
+
+const ingredientsMarkup = ingredients.map((ingredient) => {
   const newIngredient = document.createElement("li");
   newIngredient.textContent = ingredient;
   newIngredient.classList.add("item");
-  ingredientsList.appendChild(newIngredient);
+  return newIngredient;
 });
+
+ingredientsList.append(...ingredientsMarkup);
